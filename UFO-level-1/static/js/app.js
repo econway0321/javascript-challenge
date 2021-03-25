0d52457a4e09a949771be4ca912dae486efcce23
econway0321 <econway0321@gmail.com>
2021-03-24 21:44:10 -0600

    table data

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -79,6 +79,20 @@ function runEnter() {
 
   console.log(filteredData);
 
-};
+  var table = d3.select("tbody");
+
+  table.html("");
+
+  filteredData.forEach(function(ufoReport) {
+	console.log(ufoReport);
+	var row = tbody.append("tr");
+	Object.entries(ufoReport).forEach(function([key,value]) {
+		console.log(key,value);
+		var cell = row.append("td");
+		cell.text(value);
+	});
+});
+};  
+
 
 // YOUR CODE HERE!
