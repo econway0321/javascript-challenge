b68911fcddc9f25f98459097cf97e5bfb9f268d2
econway0321 <econway0321@gmail.com>
2021-03-24 21:27:48 -0600

    xxxx

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -50,4 +50,35 @@ tableData.forEach(function(ufoReport) {
 	});
 });
 
+//Filter
+
+//select the button
+var button = d3.select("#filter-btn");
+
+//select the form
+var form = d3.select("#datetime");
+
+//create event handlers
+button.on("click",runEnter);
+form.on("submit",runEnter);
+
+//complete the event handler function for the form
+function runEnter() {
+
+  //prevent the page from refreshing
+  d3.event.preventDefault();
+  //select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+  //get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  console.log(tableData);
+
+  var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
+
+  console.log(filteredData);
+
+};
+
 // YOUR CODE HERE!
