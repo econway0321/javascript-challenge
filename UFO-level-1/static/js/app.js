737f033481607d0d1ccd23a217c5362e7f54128c
econway0321 <econway0321@gmail.com>
2021-03-24 20:37:55 -0600

    table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -8,7 +8,46 @@ var tbody = d3.select("tbody");
 console.log(tableData)
 
 //Loop through data and console.log each ufo report object
+// tableData.forEach(function(ufoReport) {
+// 	console.log(ufoReport);
+// });
 
+//Use d3 to append one table row 'tr' for each ufo report object
+// tableData.forEach(function(ufoReport) {
+// 	console.log(ufoReport);
+// 	var row = tbody.append("tr");
+// });
 
+//Use Object.entries to console.log each ufo report value
+// tableData.forEach(function(ufoReport) {
+// 	console.log(ufoReport);
+// 	var row = tbody.append("tr");
+
+// 	Object.entries(ufoReport).forEach(function([key, value]) {
+// 	 console.log(key, value);
+// 	});
+// });
+
+//Use d3 to append 1 cell per ufo report value
+// tableData.forEach(function(ufoReport) {
+// 	console.log(ufoReport);
+// 	var row = tbody.append("tr");
+// 	Object.entries(ufoReport).forEach(function([key, value]) {
+// 	 console.log(key, value);
+// 	 var cell = row.append("td");
+// 	});
+// });
+
+//Use d3 to update each cell's text with ufo report values
+
+tableData.forEach(function(ufoReport) {
+	console.log(ufoReport);
+	var row = tbody.append("tr");
+	Object.entries(ufoReport).forEach(function([key,value]) {
+		console.log(key,value);
+		var cell = row.append("td");
+		cell.text(value);
+	});
+});
 
 // YOUR CODE HERE!
